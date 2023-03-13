7f1114f29e04f6a1efbc5f146f022233b5b5d74f
artin4n <julian.mjm.300@gmail.com>
2023-03-13 13:56:52 -0700

    added header file for game state

---
+++ include/game.h
@@ -0,0 +1,64 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "raylib.h"
+
+#include "lizard.h"
+#include "food.h"
+//#include "timer.h"
+
+//-- SCREEN DIMENSIONS --
+#define SCREEN_WIDTH 1600
+#define SCREEN_HEIGHT 800
+#define cells_x 20
+#define cells_y 20
+#define cell_size 25
+//-----------------------
+
+// Enum to designate menu, win, and play screens.
+typedef enum ScreenEvent { MENU = 0, PLAY, GAMEOVER } ScreenEvent;
+
+// Enum for color palette colors.
+typedef enum PaletteColors { BACKGROUND = 0, FOREGROUND, PRIMARY, SECONDARY, BASICBLACK, BASICWHITE } PaletteColors;
+
+// The game state stores all relevant data to how to program can adapt and change.
+typedef struct {
+    Lizard lizard;
+
+    Food food;
+
+    float time;
+
+    bool close;
+
+    ScreenEvent screen_event;
+
+    Sound menuSelect;
+
+    Color palette[6];
+
+    Rectangle play_area;
+} Game;
+
+// Creates a default game state. Returns a Game struct.
+Game initial_game_state();
+
+void reset_game(Game* game);
+
+void handle_input(Game* game, float dt);
+
+void step_physics(Game* game, float dt);
+
+// Draws game clock
+void draw_time(float time, Color start_color, Color end_color);
+
+void draw_play(Game game);
+
+void draw_menu(Game game);
+
+void draw_game(Game game);
+
+// Unloads all allocated resources in game
+void unload_resources(Game* game);
+
+#endif
