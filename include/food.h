1cbb58d674178b7d0010f8983284678175283326
artin4n <julian.mjm.300@gmail.com>
2023-03-13 13:56:16 -0700

    added header file for food struct

---
+++ include/food.h
@@ -0,0 +1,32 @@
+#ifndef FOOD_H
+#define FOOD_H
+
+#include "raylib.h"
+#include "lizard.h"
+
+//-- SCREEN DIMENSIONS --
+#define SCREEN_WIDTH 1600
+#define SCREEN_HEIGHT 800
+#define cells_x 20
+#define cells_y 20
+#define cell_size 25
+//-----------------------
+
+// Food is a single cell.
+// When a lizard runs over the food, it eats the food.
+// When food is eaten, a new food is generated randomly
+typedef struct { 
+    Vector2 cellPosition;
+
+    Color color;
+} Food;
+
+
+// Creates a default food. Returns a food struct.
+Food create_food(Color color);
+
+void regenerate_food(Food* food, Lizard lizard);
+
+void draw_food(Food food);
+
+#endif
